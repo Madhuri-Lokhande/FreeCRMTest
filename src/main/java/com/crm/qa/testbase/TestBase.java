efce20200302eb603e3ef31357fd0ea14372fac0
Madhuri-Lokhande <lokhande8madhuri@email.com>
2021-01-18 15:10:25 +0000

    Pushing FreeCrmProject of POM to gitHub

---
+++ src/main/java/com/crm/qa/testbase/TestBase.java
@@ -0,0 +1,72 @@
+package com.crm.qa.testbase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import com.crm.qa.utils.TestUtil;
+import com.crm.qa.utils.WebEventListener;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	public static Properties prop;
+	public  static EventFiringWebDriver e_driver;
+	public static WebEventListener eventListener;
+	
+	public TestBase() {
+		//Reading your properties from config.properties
+		try {
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream("D:/Software Testing Hands On/Automation Programs/FreeCRMTest/src/main/java/com/crm"
+					+ "/qa/config/config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void initialization() {
+		String browserName=prop.getProperty("Browser");
+		
+		if(browserName.equals("chrome")){
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\lokha\\Dropbox\\My PC (Madhuri)\\Downloads\\chromedriver_win32\\chromedriver.exe");	
+			driver = new ChromeDriver(); 
+		}
+		else if(browserName.equals("FF")){
+			System.setProperty("webdriver.gecko.driver", "C:\\Users\\lokha\\Dropbox\\My PC (Madhuri)\\Downloads\\geckodriver-v0.28.0-win64\\geckodriver.exe");	
+			driver = new FirefoxDriver(); 
+		}
+		
+		e_driver = new EventFiringWebDriver(driver);
+		// Now create object of EventListerHandler to register it with EventFiringWebDriver
+		eventListener = new WebEventListener();
+		e_driver.register(eventListener);
+		driver = e_driver;
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+		driver.get(prop.getProperty("url"));
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
